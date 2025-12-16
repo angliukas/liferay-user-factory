4e2079896c514d1f6ac02b19189e1b59414f6c43
angliukas <44555622+angliukas@users.noreply.github.com>
2025-12-16 23:44:53 +0200

    Display user IDs and creation dates in results

--- src/main/java/com/example/liferayuserfactory/model/LiferayUser.java
+++ src/main/java/com/example/liferayuserfactory/model/LiferayUser.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "User_")
 public class LiferayUser {
@@ -15,6 +17,9 @@ public class LiferayUser {
 
     @Column(name = "emailAddress")
     private String emailAddress;
+
+    @Column(name = "createDate")
+    private LocalDateTime createDate;
 
     public Long getId() {
         return id;
@@ -30,5 +35,13 @@ public class LiferayUser {
 
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
     }
 }
